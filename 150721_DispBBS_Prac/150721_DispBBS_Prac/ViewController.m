791404f55416f67f07a3fa72b34e1b04b3261224
shoshino21 <shoshino21@gmail.com>
2015-07-21 17:51:01 +0800

    Create DispBBS_Prac

---
+++ 150721_DispBBS_Prac/150721_DispBBS_Prac/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  150721_DispBBS_Prac
+//
+//  Created by shoshino21 on 7/21/15.
+//  Copyright (c) 2015 shoshino21. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
