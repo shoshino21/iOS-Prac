438c690b685f3357ad51656950e6cfc7c5d0fafc
shoshino21 <shoshino21@gmail.com>
2015-11-21 01:43:36 +0800

    KKBOX P1開始實作

--- 151117_KKBOX_Prac1_SimpleCalculator/151117_KKBOX_Prac1_SimpleCalculator/ViewController.h
+++ 151117_KKBOX_Prac1_SimpleCalculator/151117_KKBOX_Prac1_SimpleCalculator/ViewController.h
@@ -8,11 +8,35 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+  SCButtonClear,
+  SCButton0, SCButton1, SCButton2, SCButton3, SCButton4,
+  SCButton5, SCButton6, SCButton7, SCButton8, SCButton9,
+  SCButtonAdd, SCButtonSub, SCButtonMul, SCButtonDiv, SCButtonEql,
+  SCButtonDot, SCButtonChangeSign
+} SCButton ;
+
 @interface ViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UILabel *numLabel1;
-@property (weak, nonatomic) IBOutlet UILabel *numLabel2;
-@property (weak, nonatomic) IBOutlet UILabel *operatorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *clearButton;
+@property (weak, nonatomic) IBOutlet UIButton *num0Button;
+@property (weak, nonatomic) IBOutlet UIButton *num1Button;
+@property (weak, nonatomic) IBOutlet UIButton *num2Button;
+@property (weak, nonatomic) IBOutlet UIButton *num3Button;
+@property (weak, nonatomic) IBOutlet UIButton *num4Button;
+@property (weak, nonatomic) IBOutlet UIButton *num5Button;
+@property (weak, nonatomic) IBOutlet UIButton *num6Button;
+@property (weak, nonatomic) IBOutlet UIButton *num7Button;
+@property (weak, nonatomic) IBOutlet UIButton *num8Button;
+@property (weak, nonatomic) IBOutlet UIButton *num9Button;
+@property (weak, nonatomic) IBOutlet UIButton *addButton;
+@property (weak, nonatomic) IBOutlet UIButton *subButton;
+@property (weak, nonatomic) IBOutlet UIButton *mulButton;
+@property (weak, nonatomic) IBOutlet UIButton *divButton;
+@property (weak, nonatomic) IBOutlet UIButton *eqlButton;
+@property (weak, nonatomic) IBOutlet UIButton *dotButton;
+@property (weak, nonatomic) IBOutlet UIButton *changeSignButton;
 
 @end
-
--- 151117_KKBOX_Prac1_SimpleCalculator/151117_KKBOX_Prac1_SimpleCalculator/ViewController.m
+++ 151117_KKBOX_Prac1_SimpleCalculator/151117_KKBOX_Prac1_SimpleCalculator/ViewController.m
@@ -6,28 +6,176 @@
 //  Copyright (c) 2015 shoshino21. All rights reserved.
 //
 
+#define ADD_DIGIT(x) [self.resultText appendFormat:@"%d", x]
+
 #import "ViewController.h"
 
 @interface ViewController ()
 
+@property (copy, nonatomic) NSMutableString *resultText;
+@property (strong, nonatomic) NSDecimalNumber *leftDecNumber;
+@property (strong, nonatomic) NSDecimalNumber *rightDecNumber;
+
 @end
 
-@implementation ViewController
+@implementation ViewController {
+  SEL _savedSelector;
+  BOOL _hasDot;
+  BOOL _shouldClearInput;
+  BOOL _isLastOperatorEql;
+}
 
 #pragma mark - View
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+
+  [self.resultText setString:@""];
+  self.leftDecNumber = nil;
+  self.rightDecNumber = nil;
+
+  _savedSelector = nil;
+  _hasDot = NO;
+  _shouldClearInput = NO;
+  _isLastOperatorEql = NO;
+
+  self.clearButton.tag = SCButtonClear;
+  self.num0Button.tag = SCButton0;
+  self.num1Button.tag = SCButton1;
+  self.num2Button.tag = SCButton2;
+  self.num3Button.tag = SCButton3;
+  self.num4Button.tag = SCButton4;
+  self.num5Button.tag = SCButton5;
+  self.num6Button.tag = SCButton6;
+  self.num7Button.tag = SCButton7;
+  self.num8Button.tag = SCButton8;
+  self.num9Button.tag = SCButton9;
+  self.addButton.tag = SCButtonAdd;
+  self.subButton.tag = SCButtonSub;
+  self.mulButton.tag = SCButtonMul;
+  self.divButton.tag = SCButtonDiv;
+  self.eqlButton.tag = SCButtonEql;
+  self.dotButton.tag = SCButtonDot;
+  self.changeSignButton.tag = SCButtonChangeSign;
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+#pragma mark - Methods
+
+- (void)performOperation:(SCButton)operation {
+
+}
+
+- (NSMutableString *)readableNumberFromString:(NSString *)aString {
+  // given 12.30000 we remove the trailing zeros
+  NSMutableString *result = [NSMutableString stringWithString:aString];
+
+  // check if it contains a . character.
+  if ([result rangeOfString:@"."].location != NSNotFound) {
+
+    // start from the end, and remove any 0 or . you find until you find a number greater than 0
+    for (int i = (int)[result length] - 1; i >= 0; i--) {
+      // get the char
+      unichar currentChar = [result characterAtIndex:i];
+
+      if (currentChar == '0') {
+        // remove it from the string
+        [result replaceCharactersInRange:NSMakeRange(i, 1) withString:@""];
+      } else if (currentChar == '.') {
+        // remove it from the string
+        [result replaceCharactersInRange:NSMakeRange(i, 1) withString:@""];
+        break;
+      }
+      else {
+        break;
+      }
+    }
+  }
+
+  // assign default value if needed
+  if ([result isEqualToString:@""]) {
+    [result appendString:@"0"];
+  }
+
+  // remove the initial 0 if present
+  if ([result length] > 1 && [result characterAtIndex:0] == '0') {
+    [result replaceCharactersInRange:NSMakeRange(0, 1) withString:@""];
+  }
+
+  return result;
 }
 
 #pragma mark - Actions
 
+- (IBAction)numberButtonPressed:(UIButton *)sender {
+  if (_shouldClearInput) {
+    self.resultLabel.text = @"";
+    [self.resultText setString:@""];
+    _shouldClearInput = NO;
+  }
 
+  if (_isLastOperatorEql) {
+    [self p_reset];
+  }
+
+  switch ([sender tag]) {
+#warning 待補完
+    case SCButton0:
+      if ([self.resultText isEqualToString:@"0"] && !_hasDot) {
+
+      }
+
+    case SCButton1: ADD_DIGIT(1); break;
+    case SCButton2: ADD_DIGIT(2); break;
+    case SCButton3: ADD_DIGIT(3); break;
+    case SCButton4: ADD_DIGIT(4); break;
+    case SCButton5: ADD_DIGIT(5); break;
+    case SCButton6: ADD_DIGIT(6); break;
+    case SCButton7: ADD_DIGIT(7); break;
+    case SCButton8: ADD_DIGIT(8); break;
+    case SCButton9: ADD_DIGIT(9); break;
+
+    case SCButtonDot:
+      if (!_hasDot) {
+        [self.resultText appendString:@"."];
+        _hasDot = YES;
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+- (IBAction)operateButtonPressed:(UIButton *)sender {
+}
+
+#pragma mark - Private
+
+- (void)p_reset {
+#warning 所有東西歸零，開始新算式
+}
+
+- (void)p_appendNumber:(int)aNumber {
+//  if ([self.resultText isEqualToString:@"0"] && !_hasDot) {
+//    if (aNumber == 0) {
+//      return;
+//    } else {
+//      [self.resultText setString:[NSString stringWithFormat:@"%d", aNumber]];
+//    }
+//  } else {
+//    [self.resultText appendFormat:@"%d", aNumber];
+//  }
+//
+  if (![self.resultText isEqualToString:@"0"] || _hasDot) {
+    [self.resultText appendFormat:@"%d", aNumber];
+    return;
+  }
+
+  if (aNumber == 0) {
+    return;
+  } else {
+    [self.resultText setString:[NSString stringWithFormat:@"%d", aNumber]];
+  }
+}
 
 @end
