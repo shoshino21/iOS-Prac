1dd7fd269af150984dfd5d78e4f20ed925012ef5
shoshino21 <shoshino21@gmail.com>
2015-12-01 19:29:42 +0800

    修改ProductName

---
+++ 151201_Snake/151201_SnakeTests/_51201_SnakeTests.m
@@ -0,0 +1,40 @@
+//
+//  _51201_SnakeTests.m
+//  151201_SnakeTests
+//
+//  Created by shoshino21 on 12/1/15.
+//  Copyright (c) 2015 shoshino21. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _51201_SnakeTests : XCTestCase
+
+@end
+
+@implementation _51201_SnakeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
