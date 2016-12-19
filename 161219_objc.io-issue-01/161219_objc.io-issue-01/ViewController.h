070048089dbc61b89c604683248d3ffaad66fff5
shoshino21 <shoshino21@gmail.com>
2016-12-20 00:33:43 +0800

    objc.io-issue-01 init

---
+++ 161219_objc.io-issue-01/161219_objc.io-issue-01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  161219_objc.io-issue-01
+//
+//  Created by shoshino21 on 12/19/16.
+//  Copyright © 2016 shoshino21. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 161219_objc.io-issue-01/161219_objc.io-issue-01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  161219_objc.io-issue-01
+//
+//  Created by shoshino21 on 12/19/16.
+//  Copyright © 2016 shoshino21. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
